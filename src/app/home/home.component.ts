b2420bea81dff9c794223fa353a9643b4b22e13a
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-04-22 16:31:18 -0300

    finalizado função de busca de paciente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,16 +10,26 @@ import { MedicamentoService } from '../shared/services/medicamento.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+
   pacientes: Array<Paciente>;
   medicamentos: Array<Medicamento>;
+	busca = '';
+	buscando = false;
+	resultadoBusca: Array<Paciente>;
 
   constructor(
     private ps: PacienteService,
     private ms: MedicamentoService
   ) {
+		this.resultadoBusca = [];
     this.pacientes = ps.getPacientes();
     this.medicamentos = ms.getMedicamentos();
   }
+
+	buscarPaciente() {
+		this.buscando = this.busca.length > 0;
+		this.resultadoBusca = this.pacientes.filter(paciente => paciente.nome.includes(this.busca));
+	}
 
   getIdadePaciente(paciente: any): string {
     const anoNascimento = paciente.dataNascimento.split('-')[0];
